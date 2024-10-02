d9e1c0519c56ee67fe92b895bdcebf836c7699d8
sahalux111 <sahalal77@gmail.com>
2024-10-02 12:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,74 @@
 import time
 import requests
 from flask import Flask, render_template, request, redirect, url_for, session
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 from threading import Thread
-import mysql.connector  # MySQL Connector
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-# Database configuration for Hostinger MySQL
-db_config = {
-    'host': 'srv1672.hstgr.io',
-    'user': 'u953503039_root',
-    'password': 'Radblox!1',
-    'database': 'u953503039_radschedule'
+# Simulated database
+users = {
+    'sunil': {'password': generate_password_hash('sunilrbx'), 'role': 'admin'},
+    'arun': {'password': generate_password_hash('arunrbx'), 'role': 'admin'},
+    'shrini': {'password': generate_password_hash('shrinirbx'), 'role': 'admin'},
+    'sahal': {'password': generate_password_hash('sahalrbx'), 'role': 'admin'},
+    'anto': {'password': generate_password_hash('antorbx'), 'role': 'admin'},
+    'drmonika': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'dramit': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drshashank': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drronak': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'dranthony': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'droguntade': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drsmitha': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drnikita': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drkarim': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drfakhri': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'imugilteam': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drnamitha': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drsachin': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drvivek': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drraj': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'rdlteam': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'ishateam': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drdeepak': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drsurendar': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'ukteam': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drsnehal': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'teslagroup': {'password': generate_password_hash('1234'), 'role': 'admin'},
+    'pranamika': {'password': generate_password_hash('1234'), 'role': 'admin'},
+    'hemanth': {'password': generate_password_hash('1234'), 'role': 'admin'},
+    'diana': {'password': generate_password_hash('1234'), 'role': 'admin'},
+    'muflih': {'password': generate_password_hash('1234'), 'role': 'admin'},
+    'titus': {'password': generate_password_hash('1234'), 'role': 'admin'},
+    'arumugham': {'password': generate_password_hash('1234'), 'role': 'admin'},
+    'srijesh': {'password': generate_password_hash('1234'), 'role': 'admin'},
+    'radstar': {'password': generate_password_hash('1234'), 'role': 'admin'},
+    'drsuneel': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drsanketa': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'drranoji': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'sunilqa': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'arunqa': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'hemanthqa': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'pranamikaqa': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'muflihqa': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'dianaqa': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'srijeshqa': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'arumugamqa': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'titusqa': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'radstar': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'soma': {'password': generate_password_hash('1234'), 'role': 'doctor'},
+    'qa': {'password': generate_password_hash('qa'), 'role': 'qa_radiographer'}
 }
 
-# Helper function to create a database connection
-def get_db_connection():
-    return mysql.connector.connect(**db_config)
+available_doctors = {}
+doctor_breaks = {}
+doctor_notes = {}
 
-# Function to get a list of doctor names from the database
+# Function to get a list of doctor names
 def get_doctors():
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT username FROM users WHERE role = 'doctor'")
-    doctors = [row[0] for row in cursor.fetchall()]
-    cursor.close()
-    connection.close()
-    return doctors
+    return [user for user, details in users.items() if details['role'] == 'doctor']
 
 # Adjust time zone to Indian Standard Time (UTC+5:30)
 def get_indian_time():
@@ -45,16 +85,9 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
-    connection = get_db_connection()
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
-    user = cursor.fetchone()
-    cursor.close()
-    connection.close()
-
-    if user and user['password'] == password:  # Direct password check (no hashing)
+    if username in users and check_password_hash(users[username]['password'], password):
         session['username'] = username
-        session['role'] = user['role']
+        session['role'] = users[username]['role']
         return redirect(url_for('dashboard'))
     return 'Invalid credentials'
 
@@ -68,36 +101,26 @@ def dashboard():
     upcoming_scheduled = {}
     breaks = {}
 
-    connection = get_db_connection()
-    cursor = connection.cursor()
-
-    # Query for doctor breaks
-    cursor.execute("SELECT doctor, break_end FROM doctor_breaks")
-    doctor_breaks = cursor.fetchall()
-
-    for doctor, break_end in doctor_breaks:
-        break_end = datetime.strptime(break_end, '%Y-%m-%d %H:%M')
+    for doctor, break_end in doctor_breaks.items():
         if current_time >= break_end:
-            # Remove the doctor from breaks once the break ends
-            cursor.execute("DELETE FROM doctor_breaks WHERE doctor = %s", (doctor,))
+            start_time, end_time = available_doctors.get(doctor, (None, None))
+            if start_time and end_time:
+                start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M')
+                end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M')
+                if start_time <= current_time <= end_time:
+                    available_now[doctor] = end_time.strftime('%Y-%m-%d %H:%M')
+            del doctor_breaks[doctor]
         else:
             breaks[doctor] = break_end.strftime('%Y-%m-%d %H:%M')
 
-    # Query for available doctors
-    cursor.execute("SELECT doctor, start_time, end_time FROM doctor_availability")
-    doctor_availability = cursor.fetchall()
-
-    for doctor, start_time, end_time in doctor_availability:
+    for doctor, (start_time, end_time) in available_doctors.items():
         start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M')
         end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M')
 
-        if start_time <= current_time <= end_time and doctor not in breaks:
+        if start_time <= current_time <= end_time and doctor not in doctor_breaks:
             available_now[doctor] = end_time.strftime('%Y-%m-%d %H:%M')
         elif start_time > current_time:
             upcoming_scheduled[doctor] = (start_time.strftime('%Y-%m-%d %H:%M'), end_time.strftime('%Y-%m-%d %H:%M'))
-
-    cursor.close()
-    connection.close()
 
     if session['role'] == 'doctor':
         username = session['username']
@@ -106,12 +129,6 @@ def dashboard():
         return render_template('dashboard.html', available_now=available_now, breaks=breaks, upcoming_scheduled={})
 
     if session['role'] == 'qa_radiographer' or session['role'] == 'admin':
-        connection = get_db_connection()
-        cursor = connection.cursor()
-        cursor.execute("SELECT doctor, note FROM doctor_notes")
-        doctor_notes = dict(cursor.fetchall())
-        cursor.close()
-        connection.close()
         return render_template('dashboard.html', available_now=available_now, breaks=breaks, upcoming_scheduled=upcoming_scheduled, doctor_notes=doctor_notes)
 
 @app.route('/select_availability')
@@ -126,7 +143,7 @@ def select_availability():
 def set_availability():
     if 'username' not in session:
         return redirect(url_for('index'))
-
+    
     doctor = session['username']
     start_date = request.form['start_date']
     start_time = request.form['start_time']
@@ -136,13 +153,7 @@ def set_availability():
     availability_start = datetime.strptime(f'{start_date} {start_time}', '%Y-%m-%d %H:%M')
     availability_end = datetime.strptime(f'{end_date} {end_time}', '%Y-%m-%d %H:%M')
 
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("REPLACE INTO doctor_availability (doctor, start_time, end_time) VALUES (%s, %s, %s)",
-                   (doctor, availability_start.strftime('%Y-%m-%d %H:%M'), availability_end.strftime('%Y-%m-%d %H:%M')))
-    connection.commit()
-    cursor.close()
-    connection.close()
+    available_doctors[doctor] = (availability_start.strftime('%Y-%m-%d %H:%M'), availability_end.strftime('%Y-%m-%d %H:%M'))
 
     return redirect(url_for('dashboard'))
 
@@ -150,34 +161,68 @@ def set_availability():
 def take_break():
     if 'username' not in session:
         return redirect(url_for('index'))
-
+    
     doctor = session['username']
     break_duration = int(request.form['break_duration'])
     break_end_time = get_indian_time() + timedelta(minutes=break_duration)
 
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("REPLACE INTO doctor_breaks (doctor, break_end) VALUES (%s, %s)", (doctor, break_end_time.strftime('%Y-%m-%d %H:%M')))
-    connection.commit()
-    cursor.close()
-    connection.close()
+    doctor_breaks[doctor] = break_end_time
 
     return redirect(url_for('dashboard'))
+
+@app.route('/admin_control')
+def admin_control():
+    if 'username' not in session or session['role'] != 'admin':
+        return redirect(url_for('index'))
+    
+    doctors = get_doctors()  # Retrieve doctor names
+    return render_template('admin_control.html', users=users, doctor_notes=doctor_notes, doctors=doctors)
 
 @app.route('/add_note', methods=['POST'])
 def add_note():
     if 'username' not in session or session['role'] != 'admin':
         return redirect(url_for('index'))
-
+    
     doctor = request.form['doctor']
     note = request.form['note']
 
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("REPLACE INTO doctor_notes (doctor, note) VALUES (%s, %s)", (doctor, note))
-    connection.commit()
-    cursor.close()
-    connection.close()
+    doctor_notes[doctor] = note  # Save note for the doctor
+
+    return redirect(url_for('admin_control'))
+
+@app.route('/update_schedule', methods=['POST'])
+def update_schedule():
+    if 'username' not in session or session['role'] != 'admin':
+        return redirect(url_for('index'))
+
+    doctor = request.form['doctor']
+    start_date = request.form['start_date']
+    start_time = request.form['start_time']
+    end_date = request.form['end_date']
+    end_time = request.form['end_time']
+    
+    availability_start = datetime.strptime(f'{start_date} {start_time}', '%Y-%m-%d %H:%M')
+    availability_end = datetime.strptime(f'{end_date} {end_time}', '%Y-%m-%d %H:%M')
+
+    available_doctors[doctor] = (availability_start.strftime('%Y-%m-%d %H:%M'), availability_end.strftime('%Y-%m-%d %H:%M'))
+
+    return redirect(url_for('admin_control'))
+
+@app.route('/update_break', methods=['POST'])
+def update_break():
+    if 'username' not in session or session['role'] != 'admin':
+        return redirect(url_for('index'))
+
+    doctor = request.form['doctor']
+    break_start_date = request.form['break_start_date']
+    break_start_time = request.form['break_start_time']
+    break_end_date = request.form['break_end_date']
+    break_end_time = request.form['break_end_time']
+    
+    break_start = datetime.strptime(f'{break_start_date} {break_start_time}', '%Y-%m-%d %H:%M')
+    break_end = datetime.strptime(f'{break_end_date} {break_end_time}', '%Y-%m-%d %H:%M')
+
+    doctor_breaks[doctor] = break_end
 
     return redirect(url_for('admin_control'))
 
@@ -191,7 +236,7 @@ def ping_app():
     while True:
         try:
             # Replace with your deployed app's URL
-            requests.get('https://your-app-url.onrender.com/')
+            requests.get('https://radblox.onrender.com/')
             print("Ping successful!")
         except Exception as e:
             print(f"Ping failed: {e}")
@@ -204,4 +249,3 @@ if __name__ == '__main__':
     ping_thread.start()
 
     app.run(debug=True)
-
